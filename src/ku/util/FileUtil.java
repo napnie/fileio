c223effa4a03c661b17babab562009617e8768bb
Nitith Chayakul <c.nitith@gmail.com>
2017-03-16 08:41:57 +0700

    make finally block and add exception information

--- src/ku/util/FileUtil.java
+++ src/ku/util/FileUtil.java
@@ -1,12 +1,17 @@
 package ku.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class store a logic to copy from one file to another.
@@ -19,15 +24,21 @@ public class FileUtil {
 	 * Copy from one file to another byte-by-byte.
 	 * @param in - source files
 	 * @param out - withdraw files
+	 * @throws RuntimeException if I/O operation is failed or interrupt
 	 */
 	public static void copy(InputStream in, OutputStream out) {
 		try {
 			int buffer;
 			while ( (buffer = in.read()) >= 0 ) out.write(buffer);
-			in.close();
-			out.close();
 		} catch (IOException e) {
-			throw new RuntimeException();
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				in.close();
+				out.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e.getMessage());
+			}
 		}
 	}
 
@@ -36,6 +47,7 @@ public class FileUtil {
 	 * @param in - source files
 	 * @param out - withdraw files
 	 * @param blocksize - byte that copy at one
+	 * @throws RuntimeException if I/O operation failed or interrupt
 	 */
 	public static void copy(InputStream in, OutputStream out, int blocksize) {
 		try {
@@ -44,16 +56,21 @@ public class FileUtil {
 			while ( (length = in.read(buffer)) >= 0) {
 				out.write(buffer, 0, length);
 			}
-			in.close();
-			out.close();
 		} catch (IOException e) {
-			throw new RuntimeException();
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				in.close();
+				out.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e.getMessage());
+			}
 		}
 	}
 
 	/**
 	 * Copy file using BufferedReader and PrintWriter.
-	 * @see ku.util.FileUtil#copy(InputStream, OutputStream)
+	 * @see ku.util.FileUtil#copy
 	 */
 	public static void bcopy(InputStream in, OutputStream out) {
 		Reader reader = new InputStreamReader( in );
@@ -61,26 +78,26 @@ public class FileUtil {
 		String line = null;
 		PrintWriter writer = new PrintWriter( out );
 
+		List<String> longString = new ArrayList<String>();
+		
 		try {
 			while( (line = br.readLine()) != null ) {
+				longString.add(line);
 				writer.println(line);
 			}
-			in.close();
-			out.close();
-			reader.close();
-			br.close();
-			writer.close();
 		} catch (IOException e) {
-			throw new RuntimeException();
+			throw new RuntimeException( e.getMessage() );
+		} finally {
+			try {
+				in.close();
+				out.close();
+				reader.close();
+				br.close();
+				writer.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e.getMessage());
+			}
 		}
 	}
 	
-//	public static void charCopy(InputStream in, OutputStream out) {
-//		try {
-//			
-//		} catch (IOException e) {
-//			throw new RuntimeException();
-//		}
-//	}
-
 }
